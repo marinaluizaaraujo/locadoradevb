57a6a22f01c7cfbc9474178a963bb44dfd6d2bc5
Marina Luiza Paiva <mluizadearaujopaiva@gmail.com>
2024-04-25 11:54:38 -0300

    primeira aula finalizada

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- link do CSS -->
+    <link rel="stylesheet" href="./css/estilo.css">
+    <title>LOCADORA DEV</title>
+</head>
+<body>
+    <header>
+        <h1>
+        Locadora Dev
+        </h1>
+        <ul>
+            <li>
+                <a href="index.php?menu=home"> Home </a>
+            </li>
+            <li>
+                <a href="index.php?menu=lista-videos"> Vídeos </a>
+            </li>
+            <li>
+                <a href="index.php?menu=lista-categorias"> Categorias </a>
+            </li>
+            <li>
+                <a href="index.php?menu=lista-clientes"> Clientes </a>
+            </li>
+            <li>
+                <a href="index.php?menu=locacoes"> Locações </a>
+            </li>
+        </ul>
+    </header>
+</body>
+</html>
